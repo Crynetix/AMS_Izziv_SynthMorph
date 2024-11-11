b758d4496d4f9dd646b427434942d218d32e9cce
Crynetix <mh7774@student.uni-lj.si>
2024-11-12 00:56:24 +0100

    First successfull model train and registration

--- Code/Test/Tutorial_run.py
+++ Code/Test/Tutorial_run.py
@@ -92,3 +92,92 @@ print('displacement tensor:', disp_tensor.shape)
 
 # using keras, we can easily form new models via tensor pointers
 def_model = tf.keras.models.Model(unet.inputs, disp_tensor)
+
+# build transformer layer
+spatial_transformer = vxm.layers.SpatialTransformer(name='transformer')
+
+# extract the first frame (i.e. the "moving" image) from unet input tensor
+moving_image = tf.expand_dims(unet.input[..., 0], axis=-1)
+
+# warp the moving image with the transformer
+moved_image_tensor = spatial_transformer([moving_image, disp_tensor])
+
+outputs = [moved_image_tensor, disp_tensor]
+vxm_model = tf.keras.models.Model(inputs=unet.inputs, outputs=outputs)
+
+# build model using VxmDense
+inshape = x_train.shape[1:]
+vxm_model = vxm.networks.VxmDense(inshape, nb_features, int_steps=0)
+
+print('input shape: ', ', '.join([str(t.shape) for t in vxm_model.inputs]))
+print('output shape:', ', '.join([str(t.shape) for t in vxm_model.outputs]))
+
+# voxelmorph has a variety of custom loss classes
+losses = [vxm.losses.MSE().loss, vxm.losses.Grad('l2').loss]
+
+# usually, we have to balance the two losses by a hyper-parameter
+lambda_param = 0.05
+loss_weights = [1, lambda_param]
+
+vxm_model.compile(optimizer='Adam', loss=losses, loss_weights=loss_weights)
+
+def vxm_data_generator(x_data, batch_size=32):
+    """
+    Generator that takes in data of size [N, H, W], and yields data for
+    our custom vxm model. Note that we need to provide numpy data for each
+    input, and each output.
+
+    inputs:  moving [bs, H, W, 1], fixed image [bs, H, W, 1]
+    outputs: moved image [bs, H, W, 1], zero-gradient [bs, H, W, 2]
+    """
+
+    # preliminary sizing
+    vol_shape = x_data.shape[1:] # extract data shape
+    ndims = len(vol_shape)
+    
+    # prepare a zero array the size of the deformation
+    # we'll explain this below
+    zero_phi = np.zeros([batch_size, *vol_shape, ndims])
+    
+    while True:
+        # prepare inputs:
+        # images need to be of the size [batch_size, H, W, 1]
+        idx1 = np.random.randint(0, x_data.shape[0], size=batch_size)
+        moving_images = x_data[idx1, ..., np.newaxis]
+        idx2 = np.random.randint(0, x_data.shape[0], size=batch_size)
+        fixed_images = x_data[idx2, ..., np.newaxis]
+        inputs = [moving_images, fixed_images]
+        
+        # prepare outputs (the 'true' moved image):
+        # of course, we don't have this, but we know we want to compare 
+        # the resulting moved image with the fixed image. 
+        # we also wish to penalize the deformation field. 
+        outputs = [fixed_images, zero_phi]
+        
+        yield (inputs, outputs)
+
+
+# let's test it
+train_generator = vxm_data_generator(x_train)
+in_sample, out_sample = next(train_generator)
+
+# visualize
+images = [img[0, :, :, 0] for img in in_sample + out_sample] 
+titles = ['moving', 'fixed', 'moved ground-truth (fixed)', 'zeros']
+ne.plot.slices(images, titles=titles, cmaps=['gray'], do_colorbars=True);
+
+nb_epochs = 30
+steps_per_epoch = 100
+hist = vxm_model.fit_generator(train_generator, epochs=nb_epochs, steps_per_epoch=steps_per_epoch, verbose=2);
+
+import matplotlib.pyplot as plt
+
+def plot_history(hist, loss_name='loss'):
+    # Simple function to plot training history.
+    plt.figure()
+    plt.plot(hist.epoch, hist.history[loss_name], '.-')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.show()
+
+plot_history(hist)
